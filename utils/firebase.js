05b370ad0c9a4ec98f94a349170e3a70cae9768f
Shashwat <aguywhoisaheroforfun@gmail.com>
2025-07-22 14:36:23 +0530

    Update firebase.js
    removed api

--- utils/firebase.js
+++ utils/firebase.js
@@ -5,12 +5,7 @@ import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 // TODO: Replace with your Firebase project config
 const firebaseConfig = {
-  apiKey: "AIzaSyAF8GkiqW7C9kh1Ss5AAveXZZr4rcmOY8U",
-  authDomain: "ezournals.firebaseapp.com",
-  projectId: "ezournals",
-  storageBucket: "ezournals.firebasestorage.app",
-  messagingSenderId: "809838918420",
-  appId: "1:809838918420:web:c6f5bf07da87554eb6a001"
+  //Enter your own
 };
 
 const app = initializeApp(firebaseConfig);
